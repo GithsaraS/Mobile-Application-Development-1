5024d43c32fca19f6c137b144df28c4833a3a76a
GithsaraS <Ishancharun@icloud.com>
2023-04-04 23:46:19 +0530

    mad project 1

---
+++ app/src/main/java/com/example/moneytracker/edit_food.kt
@@ -0,0 +1,11 @@
+package com.example.moneytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edit_food : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_food)
+    }
+}
---
+++ app/src/main/java/com/example/moneytracker/user_account.kt
@@ -0,0 +1,11 @@
+package com.example.moneytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class user_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_account)
+    }
+}
